48e9338c44ef98ea7f3a028599431e5f53a1dd9a
pbaylis <pbaylis@gmail.com>
2014-02-08 12:27:48 -0800

    section 3 updates

--- section-03/sec-03.R
+++ section-03/sec-03.R
@@ -1,45 +1,93 @@
+rm(list=ls())
+(m <- matrix(c(1:10, 11:20), nrow = 2, ncol = 10, byrow=T))
 
-data <- read.csv("../data/auto.csv", header=TRUE)
+apply(m, MARGIN = 1, FUN = mean)
 
-names(data)
+apply(m, 2, mean)
 
+apply(m, 1:2, function(x) {x/2})
+
+rm(list=ls())
+require(foreign)
+data <- read.csv("auto.csv", header=TRUE)
 names(data) <- c("price", "mpg", "weight")
 
-head(data)
-
-head(data$mpg)
-
-lm(price ~ 1 + mpg + weight, data=data)
-
-attach(data)
-head(mpg)
-
-y <- matrix(price)
-X <- cbind(1, mpg, weight)
+y <- matrix(data$price)
+X <- cbind(1, data$mpg, data$weight)
+head(X)
 
 dim(X)[1] == nrow(y)
 
-detach(data)
+b <- solve(t(X) %*% X) %*% t(X) %*% y
+b
 
-beta <- solve(t(X) %*% X) %*% t(X) %*% y
-print(beta)
+lm(y ~ X - 1)
 
-n <- nrow(y)
-P <- X %*% solve(t(X) %*% X) %*% t(X)
-M <- diag(n) - P
+randomMat <- function(n, k) {
+  v <- rnorm(n*k)
+  matrix(v, nrow=n, ncol=k)
+}
 
-all.equal(M, t(M))
+randomMat(3,2)
 
-all.equal(M, t(M), tol=0)
+demeanMat <- function(n) {
+  ones <- rep(1, n)
+  diag(n) - (1/n) * ones %*% t(ones)
+}
 
-all.equal(M, M %*% t(M))
+A <- demeanMat(3)
+B <- matrix(1:9, nrow=3)
+col.means <- apply(B, 2, mean)
+C <- apply(B, 1, function(x) {x - col.means})
+all.equal(A %*% B, t(C))
 
-e <- M %*% y
-y.hat <- P %*% y
-rss <- t(e) %*% e
-ess <- t(y.hat) %*% y.hat
-tss <- t(y) %*% y
+data <- read.csv("auto.csv", header=TRUE)
+names(data) <- c("price", "mpg", "weight")
+y <- matrix(data$price)
+X2 <- cbind(data$mpg, data$weight)
+n <- nrow(X2)
 
-all.equal(tss, ess + rss)
+R.squared <- function(y, X) {
+  n <- nrow(X)
+  A <- demeanMat(n)
+  xtax <- t(X) %*% A %*% X
+  ytay <- t(y) %*% A %*% y
+  b2 <- solve(xtax) %*% t(X) %*% A %*% y
+  R2 <- t(b2) %*% xtax %*% b2 / ytay
+  return(R2)
+  }
 
-r
+R.squared.adj <- function(y, X) {
+  n <- nrow(X)
+  k <- ncol(X)
+  A <- demeanMat(n)
+  xtax <- t(X) %*% A %*% X
+  ytay <- t(y) %*% A %*% y
+  b2 <- solve(xtax) %*% t(X) %*% A %*% y
+  R2 <- t(b2) %*% xtax %*% b2 / ytay
+  R2.adj <- 1 - ((n-1)/(n-k))*(1-R2)
+  return(R2.adj)
+  }
+
+  R.squared(y, X2)
+  R.squared.adj(y, X2)
+
+#png(filename="inserts/graph1.png",height=300,width=500)
+n <- nrow(X2); k.max <- 100
+X.rnd <- randomMat(n, k.max)
+res.R2 <- rep(0, k.max)
+res.adjR2 <- rep(0, k.max)
+
+for (i in 1:k.max) {
+  set.seed(i)
+  X.ext <- cbind(X2, X.rnd[, seq(i)])
+  #X.ext <- cbind(X2, rnorm(n))
+  res.R2[i] <- R.squared(y, X.ext)
+  res.adjR2[i] <- R.squared.adj(y, X.ext)
+}
+
+plot(res.R2, type = "l", lwd = 3, col = "blue",
+xlab = "num. of additional columns", ylab = "R-squared value", ylim=c(0,1))
+lines(res.adjR2, type = "l", lwd = 3, col = "red")
+legend(0,1,c("R^2","adj R^2"), lty = c(1,1), lwd = c(3,3), col = c("red","blue"))
+#dev.off()
