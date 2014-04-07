c39bc3bd5647c6946bf0a1df7914f8e89a5d203e
pbaylis <pbaylis@gmail.com>
2014-04-07 10:38:19 -0700

    09 fixes for next year

--- section-09/sec-09.R
+++ section-09/sec-09.R
@@ -35,7 +35,7 @@ head(data.df, n = 3)
 class(data.df$int_date)
 
 data.df$int_date.str <- as.character(data.df$int_date)
-data.df$int_date.date <- as.Date(data.df$int_date.str, format = "%y%d%m")
+data.df$int_date.date <- as.Date(data.df$int_date.str, format = "%m%d%y")
 class(data.df$int_date.date)
 
 dow <- weekdays(data.df$int_date.date)
@@ -59,12 +59,12 @@ data.df$year <- as.numeric(format(data.df$int.date, "%Y"))
 merged.df <- merge(x = data.df, y = oilgas.yearly, by = c("year", "state"), all.x = T)
 head(merged.df)
 
-png(filename="inserts/fig1.png",height=600,width=800)
+#png(filename="inserts/fig1.png",height=600,width=800)
 library(ggplot2)
 merged.df$q1f <- factor(merged.df$q1f, levels = c("Yes", "Mixed", "No", "Don't know"))
-ggplot(merged.df, aes(x = state, fill = q1f)) + geom_bar(position = "fill") +
+ggplot(merged.df, aes(x = state, fill = q1f)) + geom_bar() +
   coord_flip() + theme(axis.text.y=element_text(size=rel(0.8)))
-dev.off()
+#dev.off()
 
 merged.df$q1b <- ifelse(merged.df$q1f == "Yes", 1, 0)
 bystate.df <- aggregate(cbind(merged.df$q1b,merged.df$emp),
